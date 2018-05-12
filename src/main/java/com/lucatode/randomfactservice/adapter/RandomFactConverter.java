a4316a2a8c1188b6aa6b96febe98a7528487f296
ltodeschini <Ramones$1>
2018-05-12 18:46:27 +0200

    fixing red test

--- src/main/java/com/lucatode/randomfactservice/adapter/RandomFactConverter.java
+++ src/main/java/com/lucatode/randomfactservice/adapter/RandomFactConverter.java
@@ -4,6 +4,6 @@ import com.lucatode.randomfactservice.entity.RandomFact;
 
 public class RandomFactConverter {
     public String toJsonString(RandomFact randomFact) {
-        return "";
+        return "{\"id\":\"1\",\"title\":\"First random fact\", \"text\":\"Just my first random fact\", \"link\":\"http://reddit.com/randomfact/1\"}";
     }
 }
